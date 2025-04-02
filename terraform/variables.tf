635f3d16124c4cc237b637591a434379e8e0437c
Juan Ceballos <juanestebanceballos@gmail.com>
2025-04-02 07:46:38 -0500

    Se queman variables para probar uso

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,8 +1,16 @@
-variable "db_username" {}
-variable "db_password" {}
+variable "db_username" {
+  default = "postgres"
+}
+variable "db_password" {
+  default = "pV1sy1pNbKVQJBYRiNMY"
+}
 
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+variable "aws_access_key" {
+  default = "AKIA4642RZANICBE7XE6"
+}
+variable "aws_secret_key" {
+  default = "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"
+}
 
 variable "db_name" {
   default = "nequi_franquicias_db"
