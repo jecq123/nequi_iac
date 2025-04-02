9f5d8db624bf9911ea1a8f6496d2cbeb215ec68b
Juan Ceballos <juanestebanceballos@gmail.com>
2025-04-02 07:47:45 -0500

    Se queman variables para probar uso, las de bd

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,12 +5,6 @@ variable "db_password" {
   default = "pV1sy1pNbKVQJBYRiNMY"
 }
 
-variable "aws_access_key" {
-  default = "AKIA4642RZANICBE7XE6"
-}
-variable "aws_secret_key" {
-  default = "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"
-}
 
 variable "db_name" {
   default = "nequi_franquicias_db"
